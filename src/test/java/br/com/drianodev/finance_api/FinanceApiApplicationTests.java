1b11ed756c5929b651b792f767ec6e4e435ab0b4
drianodev <adrianomendes661@gmail.com>
2024-10-06 10:37:49 -0300

    initial commit

---
+++ src/test/java/br/com/drianodev/finance_api/FinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drianodev.finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
